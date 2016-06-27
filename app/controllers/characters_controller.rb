8360fde471e6c95b587247123f916f75a8f7f422
yerasho <gwhuff@unomaha.edu>
2016-06-27 07:36:33 +0000

    Typeahead talks to db, need to polish characters_controler to return correct fields

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,11 +2,23 @@
 class CharactersController < ActionController::API
     
     def index
-        #params[:climate] is the value entered for Climate, or nil
-        #params[:terrain] is the value entered for Terrain, or nil
-        #params[:target] is the field in user focus
-        @array = Character.all()
-        render :json => @array
+        #params['climate'] is the value entered for Climate, or nil
+        #params['terrain'] is the value entered for Terrain, or nil
+        #params['target'] is the field in user focus
+        if params['target'] == 'climate'
+            if params['terrain']
+                array = Character.where(name: params['terrain'])
+            else
+                array = Character.all
+            end
+        elsif params['target'] == 'terrain'
+            if params['climate']
+                array = Character.where(name: params['climate'])
+            else
+                array = Character.all
+            end
+        end
+        render :json => array
     end
     
 end
