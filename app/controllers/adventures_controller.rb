0e0d5bd944f4e4b0d32c622f9515c8520386072b
yerasho <gwhuff@unomaha.edu>
2016-07-06 02:20:09 +0000

    Updated Generate! button to pass form

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -3,6 +3,10 @@ require 'MapGenerator'
 # Controller class for Adventures
 class AdventuresController < ApplicationController
     
+    def character_params
+        params.require(:character).permit(:terrain, :climate)
+    end
+  
     def index
         @grid = MapGenerator.generateDungeon
     end
